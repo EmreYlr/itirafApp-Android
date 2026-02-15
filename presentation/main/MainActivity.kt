05c1beb48d9a60f7ff09e1736b281ceaf7499535
Emre Yeler <yeleremre@hotmail.com>
2026-02-15 17:33:06 +0300

    feat: enable edge-to-edge display by adjusting window decor fitting

--- presentation/main/MainActivity.kt
+++ presentation/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             ItirafApp()
